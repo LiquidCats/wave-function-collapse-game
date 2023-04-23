c2e3c224a2e8b3dd62f3cd32a1189b5f8a58ecf0
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-04-23 18:56:24 +0400

    add new tile type for testing

--- src/core/enums/tile.ts
+++ src/core/enums/tile.ts
@@ -1,13 +1,15 @@
 export const TILE_SIZE = 50
+export const MAP_TILES = 32
+
+export const MAP_SIZE = TILE_SIZE * MAP_TILES
 
 export enum TileTypeEnum {
+    // TERRAIN
     GRASS = 'GRASS',
     TREES = 'TREES',
     BUSHES = 'BUSHES',
     SAND = 'SAND',
     WATER = 'WATER',
-}
-
-export enum InteractiveTileEnum {
-
+    // BUILDINGS
+    BUILDING_1 = 'BUILDING_1',
 }
