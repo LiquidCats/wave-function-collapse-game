fa6122bd85939d1fd3a881c401a43207e0314bdc
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-04-27 21:41:55 +0400

    todos

--- src/state/entityToPlace.ts
+++ src/state/entityToPlace.ts
@@ -1,38 +1,41 @@
 import {atom, selector} from "recoil";
 import {MAP_SIZE, TILE_SIZE} from "core/enums/tile";
 import {ENTITY_TO_OBJECT} from "core/mappers/entity";
-import {EntityBuilder} from "core/models/EntityBuilder";
 import {mapCursorCoordinatesState, tileCursorCoordinatesSelector} from "state/cursor";
 import {mapState} from "state/map";
 import {EntityTypeEnum} from "core/enums/entity";
+import Entity from "core/models/entities/Entity";
+import StructureModel from "../core/models/entities/StructureModel";
 
 export const entityToPlaceState = atom<null|EntityTypeEnum>({
     key: "entityToPlaceState",
     default: null
 })
 
-export const entityToPlaceObjectSelector = selector<EntityBuilder>({
+export const entityToPlaceObjectSelector = selector<Entity|null>({
     key: "entityToPlaceObjectSelector",
     get: ({get}) => {
         const entityType = get(entityToPlaceState)
-        if (entityType) {
-            return ENTITY_TO_OBJECT.get(entityType)
-                ?? EntityBuilder.dummy()
+        if (!entityType) {
+            return null
         }
 
-        return EntityBuilder.dummy()
+        return ENTITY_TO_OBJECT.get(entityType)
+            ?? null
     }
 })
 
-export const entityToPlaceBottomRightCoordsSelector = selector({
+export const entityToPlaceBottomRightCoordsSelector = selector<[number, number]>({
     key: "entityToPlaceBottomRightCoordsSelector",
     get: ({get}) => {
         const [x, y] = get(mapCursorCoordinatesState)
         const buildingObject = get(entityToPlaceObjectSelector)
+        if (!buildingObject) {
+            return [0, 0]
+        }
 
-
-        const localX = (x+TILE_SIZE*buildingObject.size) - TILE_SIZE/2
-        const localY = (y+TILE_SIZE*buildingObject.size) - TILE_SIZE/2
+        const localX = (x + TILE_SIZE * buildingObject.size) - TILE_SIZE/2
+        const localY = (y + TILE_SIZE * buildingObject.size) - TILE_SIZE/2
 
         return [localX, localY]
     }
@@ -54,6 +57,11 @@ export const entityToPlaceCanPlaceSelector = selector<boolean>({
     get: ({get}) => {
         const map = get(mapState)
         const buildingObject = get(entityToPlaceObjectSelector)
+        if (!buildingObject) {
+            return false
+        }
+
+
         const [tileX, tileY] = get(tileCursorCoordinatesSelector)
         const [bottomRightX, bottomRightY] = get(entityToPlaceBottomRightCoordsSelector)
         const [topLeftX,topLeftY] = get(entityToPlaceTopLeftCoordsSelector)
@@ -66,12 +74,18 @@ export const entityToPlaceCanPlaceSelector = selector<boolean>({
             for (let j = tileX; j < tileX+buildingObject.size; j++) {
                 const tile = map[i][j]
 
-                if (!buildingObject.canBePlacedOn.includes(tile)) {
-                    return false
+                if (buildingObject instanceof StructureModel) {
+                    if (!buildingObject?.canBePlacedOn?.includes(tile)) {
+                        return false
+                    }
                 }
+
             }
         }
 
         return true
     }
 })
+
+// @TODO add checks and calculation not to place entities on entities
+// @TODO add checks and calculation not to place structures close to structures
